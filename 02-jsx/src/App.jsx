27facde6711893ab32536e7e389e9e697d6eb5e1
Thomas <thomasribalta@gmail.com>
2024-12-13 18:35:55 +0100

    add img to front

--- 02-jsx/src/App.jsx
+++ 02-jsx/src/App.jsx
@@ -9,14 +9,17 @@ const App = () => {
       <Pizza
         name="The Pepperoni Pizza"
         description="Mozzarella Cheese, Pepperoni"
+        image={"/public/pizzas/pepperoni.webp"}
       />
       <Pizza
         name="The Hawaiian Pizza"
         description="Sliced Ham, Pineapple, Mozzarella Cheese"
+        image={"/public/pizzas/hawaiian.webp"}
       />
       <Pizza
         name="The Big Meat Pizza"
         description="Bacon, Pepperoni, Italian Sausage, Chorizo Sausage"
+        image={"/public/pizzas/big_meat.webp"}
       />
     </div>
   );
